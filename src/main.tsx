c62a2d16e574583f8e1e75a01685d2294259465b
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-07-05 23:12:39 +0530

    Wrapped with theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
+import ThemeProvider from "./theme/themeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+         <ThemeProvider>
         <App />
+        </ThemeProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
